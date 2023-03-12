178b72c4210ffda4c224cf85f9723b88ba22113b
lizzzu <lizudim@yahoo.com>
2023-03-12 18:39:17 +0200

    lu decomposition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.linalg as la
 
 def generate(n):
     a = np.zeros((n, n))
@@ -34,10 +35,9 @@ def inverse_substitution(a, b):
     n = len(a)
     x = np.zeros(n)
     for i in range(n - 1, -1, -1):
-        x[i] = b[i] - sum([a[i][j] * x[j] for j in range(i + 1, n)])
+        x[i] = b[i] - sum([a[j][i] * x[j] for j in range(i + 1, n)])
     return x
 
-eps = 1e-6
 a_init = np.array([
     [1.0, 2.5, 3.0],
     [2.5, 8.25, 15.5],
@@ -45,7 +45,7 @@ a_init = np.array([
 ])
 b = np.array([12, 38, 68])
 
-# a, b = generate(3)
+a, b = generate(3)
 
 a, d = decompose(a_init)
 print(d)
@@ -55,3 +55,11 @@ z = direct_substitution(a, b)
 y = z / d
 x = inverse_substitution(a, y)
 print(x)
+
+p, l, u = la.lu(a_init)
+y = la.solve(l, p @ b)
+x = la.solve(u, y)
+print(x)
+
+norm = np.sqrt(sum([i * i for i in a_init @ x - b]))
+print(norm < 1e-8)
