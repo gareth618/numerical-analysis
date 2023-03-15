971152638c7dc249b31d27115b929332eee7d8c0
lizzzu <lizudim@yahoo.com>
2023-03-15 19:04:09 +0200

    householder
    Co-authored-by: gareth618 <iulian.oleniuc@gmail.com>

--- public/python/3.py
+++ public/python/3.py
@@ -19,8 +19,38 @@ def random_system(n):
     b = a @ s
     return a, b, s
 
-def householder(a):
+def householder(a, b):
     """descompunem QR matricea A"""
+    n = len(a)
+    q_tilda = np.identity(n)
+    u = np.empty(n)
+    for r in range(n - 1):
+        sigma = sum([a[i][r] ** 2 for i in range(r, n)])
+        if sigma <= 1e-6:
+            break
+        k = np.sqrt(sigma)
+        if a[r][r] > 0:
+            k *= -1
+        beta = sigma - k * a[r][r]
+        u[r] = a[r][r] - k
+        for i in range(r + 1, n):
+            u[i] = a[i][r]
+        for j in range(r + 1, n):
+            gamma = sum([u[i] * a[i][j] for i in range(r, n)]) / beta
+            for i in range(r, n):
+                a[i][j] -= gamma * u[i]
+        a[r][r] = k
+        for i in range(r + 1, n):
+            a[i][r] = 0
+        gamma = sum([u[i] * b[i] for i in range(r, n)]) / beta
+        for i in range(r, n):
+            b[i] -= gamma * u[i]
+        for j in range(n):
+            gamma = sum([u[i] * q_tilda[i][j] for i in range(r, n)]) / beta
+            for i in range(r, n):
+                q_tilda[i][j] -= gamma * u[i]
+    print('a = ', a)
+    print('q_tilda =', q_tilda)
     q = ...
     r = ...
     return q, r
@@ -57,21 +87,21 @@ def limit(a):
 def test(n, generate_system):
     a, b, s = generate_system(n)
     print('b =', b)
-    q, r = householder(a)
-    print('Q =', q)
-    print('R =', r)
-    x_hh = solve_system(a, b, householder)
-    x_qr = solve_system(a, b, np.linalg.qr)
-    print('x_HH =', x_hh)
-    print('x_QR =', x_qr)
-    print('norm(x_HH - x_QR) =', np.linalg.norm(x_hh - x_qr))
-    print('errors(x_HH) =', system_errors(a, b, x_hh, s))
-    print('errors(x_QR) =', system_errors(a, b, x_qr, s))
-    inv_a_hh = inverse(a)
-    inv_a_np = np.linalg.inv(a)
-    print('norm(inv_A_HH - inv_A_NP) =', np.linalg.norm(inv_a_hh - inv_a_np))
-    lim = limit(random_symmetric_matrix(n))
-    print('lim =', lim)
+    q, r = householder(a, b)
+    # print('Q =', q)
+    # print('R =', r)
+    # x_hh = solve_system(a, b, householder)
+    # x_qr = solve_system(a, b, np.linalg.qr)
+    # print('x_HH =', x_hh)
+    # print('x_QR =', x_qr)
+    # print('norm(x_HH - x_QR) =', np.linalg.norm(x_hh - x_qr))
+    # print('errors(x_HH) =', system_errors(a, b, x_hh, s))
+    # print('errors(x_QR) =', system_errors(a, b, x_qr, s))
+    # inv_a_hh = inverse(a)
+    # inv_a_np = np.linalg.inv(a)
+    # print('norm(inv_A_HH - inv_A_NP) =', np.linalg.norm(inv_a_hh - inv_a_np))
+    # lim = limit(random_symmetric_matrix(n))
+    # print('lim =', lim)
 
 test(3, sample_system)
-test(5, random_system)
+# test(5, random_system)
