36f8e228e1dd0e738abf846610f2283bb3af7372
Gareth618 <bloggareth@gmail.com>
2023-03-17 08:56:32 +0200

    inverse using householder
    Co-authored-by: lizzzu <lizudim@yahoo.com>

--- public/python/3.py
+++ public/python/3.py
@@ -11,16 +11,14 @@ def sample_system(_):
     return a, b, s
 
 def random_system(n):
-    """generăm random matricea A și vectorul s, reprezentând o soluție a sistemului pe care dorim să-l creăm
-    apoi, cunoscând A, s și că s este o soluție a sistemului Ax = b, determinăm b-ul drept As
-    astfel, ne asigurăm că sistemul pe care-l generăm admite din start o soluție, și anume pe s"""
     a = np.random.uniform(-99, 100, (n, n))
     s = np.random.uniform(-99, 100, n)
     b = a @ s
     return a, b, s
 
-def householder(a, b):
-    """descompunem QR matricea A"""
+def householder(a_init, b_init):
+    a = a_init.copy()
+    b = b_init.copy()
     n = len(a)
     q_tilda = np.identity(n)
     u = np.empty(n)
@@ -49,27 +47,30 @@ def householder(a, b):
             gamma = sum([u[i] * q_tilda[i][j] for i in range(r, n)]) / beta
             for i in range(r, n):
                 q_tilda[i][j] -= gamma * u[i]
-    print('a = ', a)
-    print('q_tilda =', q_tilda)
-    q = ...
-    r = ...
+    q = np.transpose(q_tilda)
+    r = a.copy()
     return q, r
 
-def solve_system(a, b, decompose):
-    """decompose este o funcție de callback ce descompune QR matricea A
-    descompunerea poate fi cea implementată de noi (householder) sau cea din bibliotecă"""
-    q, r = decompose(a)
-    x = ...
+def inverse_substitution(a, b):
+    n = len(a)
+    x = np.empty(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - sum([a[i][j] * x[j] for j in range(i + 1, n)])) / a[i][i]
     return x
+
+def solve_system(q, r, b):
+    return inverse_substitution(r, np.transpose(q) @ b)
 
 def system_errors(a, b, x, s):
     norm1 = np.linalg.norm(a @ x - b)
     norm2 = np.linalg.norm(x - s) / np.linalg.norm(s)
     return norm1, norm2
 
-def inverse(a):
-    q, r = householder(a)
-    inv = ...
+def inverse(q, r):
+    n = len(q)
+    inv = np.empty((n, n))
+    for j in range(n):
+        inv[:, j] = inverse_substitution(r, q[j, :])
     return inv
 
 def random_symmetric_matrix(n):
@@ -80,7 +81,6 @@ def random_symmetric_matrix(n):
     return a
 
 def limit(a):
-    """folosim householder la fiecare pas"""
     lim = ...
     return lim
 
@@ -88,20 +88,23 @@ def test(n, generate_system):
     a, b, s = generate_system(n)
     print('b =', b)
     q, r = householder(a, b)
-    # print('Q =', q)
-    # print('R =', r)
-    # x_hh = solve_system(a, b, householder)
-    # x_qr = solve_system(a, b, np.linalg.qr)
-    # print('x_HH =', x_hh)
-    # print('x_QR =', x_qr)
-    # print('norm(x_HH - x_QR) =', np.linalg.norm(x_hh - x_qr))
-    # print('errors(x_HH) =', system_errors(a, b, x_hh, s))
-    # print('errors(x_QR) =', system_errors(a, b, x_qr, s))
-    # inv_a_hh = inverse(a)
-    # inv_a_np = np.linalg.inv(a)
-    # print('norm(inv_A_HH - inv_A_NP) =', np.linalg.norm(inv_a_hh - inv_a_np))
-    # lim = limit(random_symmetric_matrix(n))
-    # print('lim =', lim)
+    print('Q =', q)
+    print('R =', r)
+    x_hh = solve_system(q, r, b)
+    x_qr = solve_system(*np.linalg.qr(a), b)
+    print('x_HH =', x_hh)
+    print('x_QR =', x_qr)
+    print('norm(x_HH - x_QR) =', np.linalg.norm(x_hh - x_qr))
+    print(b)
+    print('errors(x_HH) =', system_errors(a, b, x_hh, s))
+    print('errors(x_QR) =', system_errors(a, b, x_qr, s))
+    inv_a_hh = inverse(q, r)
+    inv_a_np = np.linalg.inv(a)
+    print('inv_A_HH =', inv_a_hh)
+    print('inv_A_NP =', inv_a_np)
+    print('norm(inv_A_HH - inv_A_NP) =', np.linalg.norm(inv_a_hh - inv_a_np))
+    lim = limit(random_symmetric_matrix(n))
+    print('lim =', lim)
 
 test(3, sample_system)
 # test(5, random_system)
