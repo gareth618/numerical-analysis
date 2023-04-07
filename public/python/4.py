d42299b85462dad22e5c3b1275670994ac9380e0
lizzzu <lizudim@yahoo.com>
2023-04-07 17:11:49 +0300

    bonus 4

--- public/python/4.py
+++ public/python/4.py
@@ -1,5 +1,6 @@
 import numpy as np
 import urllib.request
+from copy import deepcopy
 
 class SparseMatrix:
     def __init__(self, n):
@@ -8,6 +9,26 @@ class SparseMatrix:
 
     def __str__(self):
         return str(self.matrix)
+
+    def __add__(self, other):
+        s = SparseMatrix(self.n)
+        s.matrix = deepcopy(self.matrix)
+        for i in range(self.n):
+            for j, val in other.matrix[i].items():
+                s.add_element(i, j, val)
+        return s
+
+    def __eq__(self, other):
+        for i in range(self.n):
+            for j, val in self.matrix[i].items():
+                if j not in other.matrix[i] and val != 0:
+                    return False
+                if j in other.matrix[i] and abs(val - other.matrix[i][j]) > 1e-8:
+                    return False
+            for j in other.matrix[i]:
+                if j not in self.matrix[i]:
+                    return False
+        return True
 
     def add_element(self, i, j, x):
         self.matrix[i][j] = self.matrix[i].setdefault(j, 0) + x
@@ -57,11 +78,8 @@ def make_file_urls(index):
     file_b = f'https://profs.info.uaic.ro/~ancai/CN/lab/4/sislinrar/b_{index + 1}.txt'
     return file_a, file_b
 
-def load_system(file_index):
-    file_a, file_b = make_file_urls(file_index)
-
-    content_a = urllib.request.urlopen(file_a).read().decode('utf-8').split('\n')
-    # content_a = open(f'a_{file_index + 1}.txt', 'r').read().split('\n')
+def load_sparse_matrix(file):
+    content_a = urllib.request.urlopen(file).read().decode('utf-8').split('\n')
     n = int(content_a[0])
     a = SparseMatrix(n)
     for line in content_a[1:]:
@@ -71,12 +89,23 @@ def load_system(file_index):
         i = int(val[1].strip())
         j = int(val[2].strip())
         a.add_element(i, j, x)
+    return a
 
+def load_system(file_index):
+    file_a, file_b = make_file_urls(file_index)
+    a = load_sparse_matrix(file_a)
     content_b = urllib.request.urlopen(file_b).read().decode('utf-8').split('\n')
-    # content_b = open(f'b_{file_index + 1}.txt', 'r').read().split('\n')
     b = [float(val) for val in content_b[1:] if len(val.strip()) > 0]
-
     return a, b
+
+def test_addition():
+    file_a = f'https://profs.info.uaic.ro/~ancai/CN/lab/4/sislinrar/a.txt'
+    file_b = f'https://profs.info.uaic.ro/~ancai/CN/lab/4/sislinrar/b.txt'
+    file_s = f'https://profs.info.uaic.ro/~ancai/CN/lab/4/sislinrar/aplusb.txt'
+    a = load_sparse_matrix(file_a)
+    b = load_sparse_matrix(file_b)
+    s = load_sparse_matrix(file_s)
+    print('a + b = s:', a + b == s)
 
 if __name__ == '__main__':
     file_count = 5
@@ -89,24 +118,26 @@ if __name__ == '__main__':
         lambda n: [2] * n
     ]
 
-    a = SparseMatrix.compress([
-        [102.5, 0, 2.5, 0, 0],
-        [3.5, 104.88, 1.05, 0, 0.33],
-        [0, 0, 100, 0, 0],
-        [0, 1.3, 0, 101.3, 0],
-        [0.73, 0, 0, 1.5, 102.23]
-    ])
+    # a = SparseMatrix.compress([
+    #     [102.5, 0, 2.5, 0, 0],
+    #     [3.5, 104.88, 1.05, 0, 0.33],
+    #     [0, 0, 100, 0, 0],
+    #     [0, 1.3, 0, 101.3, 0],
+    #     [0.73, 0, 0, 1.5, 102.23]
+    # ])
 
-    x = [1, 2, 3, 4, 5]
-    b = [6, 7, 8, 9, 1]
+    # x = [1, 2, 3, 4, 5]
+    # b = [6, 7, 8, 9, 1]
 
-    print(a.solve_system(b, x))
-    for i in range(file_count):
-        a, b = load_system(i)
-        if a.check_diagonal():
-            x = a.solve_system(b)
-            if x != 'divergence':
-                norm = np.linalg.norm(np.array(x) - np.array(make_solution[i](a.n)))
-                print(f'[{i + 1}]', 'norm = 0:', norm < 1e-6)
-            else:
-                print(f'[{i + 1}]', x)
+    # print(a.solve_system(b, x))
+    # for i in range(file_count):
+    #     a, b = load_system(i)
+    #     if a.check_diagonal():
+    #         x = a.solve_system(b)
+    #         if x != 'divergence':
+    #             norm = np.linalg.norm(np.array(x) - np.array(make_solution[i](a.n)))
+    #             print(f'[{i + 1}]', 'norm = 0:', norm < 1e-6)
+    #         else:
+    #             print(f'[{i + 1}]', x)
+
+    test_addition()
